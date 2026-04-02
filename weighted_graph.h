e4bae4fb23eac9eae151719f650edf7bede82fe9
abhiiiii927 <khushisinghneetuom@gmail.com>
2026-04-02 15:45:11 +0530

    Create weighted_graph.h

---
+++ weighted_graph.h
@@ -0,0 +1,31 @@
+#ifndef WEIGHTED_GRAPH_H
+#define WEIGHTED_GRAPH_H
+
+#include <vector>
+#include <list>
+#include <climits>
+
+struct weighted_edge {
+    int dest;
+    int weight;
+};
+
+class WeightedGraph {
+private:
+    int vertices;
+    std::vector<std::list<weighted_edge>> adjList;
+    std::vector<int> distance;
+    std::vector<int> parent;
+    std::vector<bool> solved;
+
+public:
+    WeightedGraph(int vertices);
+    void addEdge(int src, int dest, int weight);
+    void printGraph();
+    void dijkstra(int src);
+    int getDistance(int node);
+    int getParent(int node);
+    bool isSolved(int node);
+};
+
+#endif
